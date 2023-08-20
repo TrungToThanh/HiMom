6306ddf271fa6f050aa7de53354bc392a776a802
thanhtrungantin <thanhtrungantin@gmail.com>
2023-08-20 21:39:24 +0700

    feat: config

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import { View, Text } from "react-native";
+import { Button, Steps,Result , SwipeAction,List ,Accordion , Modal  } from 'antd-mobile-rn'
 
 export default function App() {
+  const Step = Steps.Step
+
+  const right = [
+    {
+      text: 'More',
+      onPress: () => console.log('more'),
+      backgroundColor: 'orange',
+      color: 'white',
+    },
+    {
+      text: 'Delete',
+      onPress: () => console.log('delete'),
+      backgroundColor: 'red',
+      color: 'white',
+    },
+  ]
   return (
+
+
     <View
       style={{
         flex: 1,
@@ -9,7 +28,16 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Universal React with Expo</Text>
+    
+      <Button >12 </Button>
+     
+      <SwipeAction
+            right={right}
+            onSwipeableOpen={() => console.log('open')}
+            onSwipeableClose={() => console.log('close')}>
+            <Text>Jennie</Text>
+          </SwipeAction>
+  
     </View>
   );
 }
