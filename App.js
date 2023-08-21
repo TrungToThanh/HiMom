bb317cc8f678c7a0b758c75d81300d0cf50d960f
Oscar.To <oscar.to@grapecity.com>
2023-08-21 15:52:18 +0900

    fix UI login

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-import { useEffect } from "react";
 import { AppLoading, Font } from "expo";
-import { View, Text, StatusBar, SafeAreaView, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StatusBar, StyleSheet } from "react-native";
 import Login from "./app/Login/Login";
 
 export default function App() {
+  // const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   const loading = async () => {
+  //     await Font.loadAsync(
+  //       "antoutline",
+  //       require("@ant-design/icons-react-native/fonts/antoutline.ttf")
+  //     );
+  //     await Font.loadAsync("antfill", require("@ant-design/icons-react-native/fonts/antfill.ttf"));
+  //     setIsLoading(false);
+  //   };
+  //   loading();
+  // }, []);
+
+  // if (isLoading) return <AppLoading />;
   return (
     <View>
       <StatusBar animated={true} backgroundColor="white" />
--- app/Login/Login.tsx
+++ app/Login/Login.tsx
@@ -1,53 +1,114 @@
-import { Radio, List, Button, InputItem, Card, WhiteSpace } from "@ant-design/react-native";
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import {
+  Radio,
+  Icon,
+  Button,
+  InputItem,
+  Card,
+  WhiteSpace,
+  Checkbox,
+} from "@ant-design/react-native";
+
+const options = [
+  { label: "1212", value: "1" },
+  { label: "1212", value: "2" },
+];
 
 export default function Login() {
   const RadioItem = Radio.RadioItem;
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
+  const [typeInput, setTypeInput] = useState(true);
   return (
     <View style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: "https://reactnative.dev/img/tiny_logo.png",
-        }}
-      />
-      <WhiteSpace />
       <View
         style={{
+          height: windowHeight / 4,
           display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-          width: "100%",
+          justifyContent: "center",
         }}
       >
-        <Card style={{ width: "100%" }}>
-          <Card.Header
-            title="Thông tin đăng nhập"
-            thumbStyle={{ width: 30, height: 30 }}
-            thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
-          />
-          <List renderHeader={"Tài khoản:"} style={{ width: "100%" }}>
-            <Radio.Group
-              style={{
-                width: "100%",
-                //   flexDirection: "row",
-                //   justifyContent: "space-around",
-                paddingVertical: 6,
-              }}
-            >
-              <Radio value={1}>A</Radio>
-              <Radio value={2}>B</Radio>
-              <Radio value={3}>C</Radio>
-              <Radio value={4}>D</Radio>
-            </Radio.Group>
-          </List>
-          <List renderHeader={"Mã đăng nhập"} style={{ width: "100%" }}>
-            <InputItem clear error placeholder="Mã đăng nhập"></InputItem>
-          </List>
-        </Card>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: "https://reactnative.dev/img/tiny_logo.png",
+          }}
+        />
       </View>
-      <Button style={{ width: "50%", backgroundColor: "primary" }}> Đăng nhập </Button>
+
+      <WhiteSpace />
+      <Card
+        style={{
+          width: windowWidth - 15,
+          padding: 10,
+        }}
+      >
+        <Card.Header
+          title={
+            <Text style={{ color: "#1870bc", fontSize: 18, fontWeight: "bold" }}>
+              Thông tin đăng nhập
+            </Text>
+          }
+          thumbStyle={{ width: 30, height: 30 }}
+          thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
+          style={{}}
+        />
+        <Card.Body>
+          <Text
+            style={{
+              color: "#1870bc",
+              fontSize: 16,
+              fontWeight: "bold",
+              paddingLeft: 20,
+            }}
+          >
+            Tài khoản:
+          </Text>
+          <Radio.Group
+            style={{
+              marginLeft: 30,
+              marginRight: 20,
+            }}
+            options={options}
+          ></Radio.Group>
+
+          <Text
+            style={{
+              color: "#1870bc",
+              fontSize: 16,
+              fontWeight: "bold",
+              paddingLeft: 20,
+              paddingTop: 20,
+            }}
+          >
+            Mã đăng nhập:
+          </Text>
+          <InputItem
+            clear
+            type={typeInput ? "password" : "text"}
+            maxLength={8}
+            placeholder="Mã đăng nhập"
+            style={{ marginLeft: 20, borderBottomWidth: 1 }}
+            extra={<Button onPress={() => setTypeInput(!typeInput)}> Show </Button>}
+          ></InputItem>
+        </Card.Body>
+        <View
+          style={{
+            width: "50%",
+            backgroundColor: "primary",
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            alignSelf: "center",
+          }}
+        >
+          <Button type="primary">
+            <Text style={{ color: "white" }}> Đăng nhập</Text>
+          </Button>
+        </View>
+      </Card>
     </View>
   );
 }
