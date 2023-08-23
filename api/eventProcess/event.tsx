4dc45709d55f07e5738e00108cb8b0637fdcb347
thanhtrungantin <thanhtrungantin@gmail.com>
2023-08-23 21:47:13 +0700

    add feat for event

---
+++ api/eventProcess/event.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import * as SQLite from "expo-sqlite";
+import dayjs from "dayjs";
+import { nameDB } from "../database";
+
+const nameTable = "tableEventProcess";
+
+export const createProcessEventTable = () => {
+  const db = SQLite.openDatabase(nameDB);
+  return new Promise(function (resolve) {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS ${nameTable} (id INTEGER PRIMARY KEY AUTOINCREMENT, event TEXT , date TEXT, description TEXT, note TEXT )`
+      );
+    });
+  });
+};
+
+export const insertANewEvent = (event, date, description, note) => {
+  const db = SQLite.openDatabase(nameDB);
+  return new Promise(function (resolve) {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS ${nameTable} (id INTEGER PRIMARY KEY AUTOINCREMENT, event TEXT , date TEXT, description TEXT, note TEXT )`
+      );
+      tx.executeSql(
+        `INSERT INTO ${nameTable} (event, date, description, note) values (?, ?, ?, ?)`,
+        [event, date, description, note],
+        (txObj, resultSet) => resolve(true),
+        (txObj, error) => false
+      );
+    });
+  });
+};
+
+export const getAllEvent = () => {
+  const [db, setDb] = useState(SQLite.openDatabase(nameDB));
+  const [listEvent, setListEvent] = useState<any>();
+  var items = new Array();
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS ${nameTable} (id INTEGER PRIMARY KEY AUTOINCREMENT, event TEXT , date TEXT, description TEXT, note TEXT )`
+      );
+      tx.executeSql(
+        `SELECT * FROM ${nameTable}`,
+        null,
+        (txObj, resultSet) => {
+          const value = resultSet.rows;
+          console.log("resultSet", resultSet);
+          var len = resultSet.rows.length;
+          for (var i = 0; i < len; i++) {
+            items.push(resultSet.rows.item(i));
+          }
+          items && items.length > 0 && setListEvent(items);
+        },
+        (txObj, error) => false
+      );
+    });
+  }, [db]);
+  return {
+    listEvent,
+  };
+};
+
+export const deleteAEvent = (id) => {
+  const db = SQLite.openDatabase(nameDB);
+  return new Promise(function (resolve) {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `DELETE FROM ${nameTable} WHERE id = ?`,
+        [Number(id)],
+        (txObj, resultSet) => resolve(true),
+        (txObj, error) => false
+      );
+    });
+  });
+};
--- app/Process/Process.tsx
+++ app/Process/Process.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import {
   Button,
   InputItem,
@@ -11,6 +18,7 @@ import {
   Steps,
   Modal,
   TextareaItem,
+  ActivityIndicator,
 } from "@ant-design/react-native";
 
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
@@ -21,18 +29,16 @@ import {
   faUser,
   faCalendar,
   faEdit,
+  faL,
 } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 import dayjs from "dayjs";
-import {
-  deleteAItemBabyFromBabyList,
-  insertValueBabyToBabyList,
-  updateValueOfABabyInBabyList,
-} from "../../api/login/login";
+
 import CardHeader from "@ant-design/react-native/lib/card/CardHeader";
 import { useNavigation } from "@react-navigation/native";
 import CardBody from "@ant-design/react-native/lib/card/CardBody";
+import { getAllEvent, insertANewEvent } from "../../api/eventProcess/event";
 
 interface Props {
   listAccountBaby?: any;
@@ -43,17 +49,23 @@ const ProcessBaby = ({ listAccountBaby, nameRouteUserId }: Props) => {
   const windowWidth = Dimensions.get("window").width;
   const windowHeight = Dimensions.get("window").height;
   const Step = Steps.Step;
+  const [isLoading, setIsLoading] = useState(false);
 
+  //Get value date
   const now = dayjs().format("DD-MM-YYYYY");
 
   const isFirstDay = useMemo(() => {
     let dateObject = "";
     if (listAccountBaby) {
-      let idCurrent = listAccountBaby?.find((item) => Number(item.id) === Number(nameRouteUserId));
+      let idCurrent = listAccountBaby?.find(
+        (item) => Number(item.id) === Number(nameRouteUserId)
+      );
       var dateParts = idCurrent?.birthday.split("-");
 
       // month is 0-based, that's why we need dataParts[1] - 1
-      dateObject = dayjs(new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]))
+      dateObject = dayjs(
+        new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0])
+      )
         .subtract(280, "days")
         .format("DD-MM-YYYY");
 
@@ -65,57 +77,90 @@ const ProcessBaby = ({ listAccountBaby, nameRouteUserId }: Props) => {
   const isBirthday = useMemo(() => {
     let valueReturn = "";
     if (listAccountBaby) {
-      let idCurrent = listAccountBaby?.find((item) => Number(item.id) === Number(nameRouteUserId));
+      let idCurrent = listAccountBaby?.find(
+        (item) => Number(item.id) === Number(nameRouteUserId)
+      );
 
       if (idCurrent) valueReturn = String(idCurrent?.birthday);
     }
     return valueReturn;
   }, [listAccountBaby, nameRouteUserId]);
 
-  const [listEvent, setListEvent] = useState<any>();
-  const [nameEvent, setNameEvent] = useState<any>();
-  const [dateEvent, setDateEvent] = useState<any>();
-  const [desEvent, setDesEvent] = useState<any>();
-  const [noteEvent, setNoteEvent] = useState<any>();
+  //Modal Add Event
+  const [isShowEvent, setShowEvent] = useState<boolean>(false);
+  const [nameEvent, setNameEvent] = useState<any>("");
+  const [dateEvent, setDateEvent] = useState<any>("");
+  const [desEvent, setDesEvent] = useState<any>("");
+  const [noteEvent, setNoteEvent] = useState<any>("");
+
+  const { listEvent } = getAllEvent();
+
+  const listEventCook = useMemo(() => {
+    const listEventCurrent = listEvent?.length > 0 ? listEvent : [];
+    const isHasFirstDay = listEventCurrent?.find(
+      (item) => Number(item.id) === -1
+    );
+    if (!isHasFirstDay)
+      listEventCurrent?.unshift({
+        id: -1,
+        event: isFirstDay,
+        description: "Nhịp đập đầu tiên!",
+        status: "finish",
+      });
+    const isHasBirthDay = listEventCurrent?.find(
+      (item) => Number(item.id) === 1000
+    );
+    if (!isHasBirthDay)
+      listEventCurrent?.push(
+        {
+          id: 1000,
+          event: now,
+          description: "Hôm nay",
+          status: "wait",
+        },
+        {
+          id: 1001,
+          event: isBirthday,
+          description: "Oa oa",
+          status: "wait",
+        }
+      );
+
+    const newList = new Set(listEventCurrent);
+    console.log(newList);
+    return Array.from(newList);
+  }, [listEvent]);
 
   const handleAddEvent = () => {
-    Modal.alert(
-      "Thêm sự kiện",
-      <View style={{ width: windowWidth }}>
-        <Text> Tên sự kiện:</Text>
-        <InputItem
-          placeholder="Tên sự kiện"
-          style={{ width: "90%", paddingLeft: -20, margin: 0 }}
-          multiline
-          textBreakStrategy="highQuality"
-          onChangeText={(value) => setNameEvent(value?.trim())}
-        ></InputItem>
-        <Text>Ngày xảy ra sự kiện:</Text>
-        <InputItem
-          placeholder="Ngày xảy ra sự kiện"
-          onChangeText={(value) => setDateEvent(value?.trim())}
-        ></InputItem>
-        <Text> Mô tả sự kiện:</Text>
-        <TextareaItem
-          rows={4}
-          placeholder="Mô tả sự kiện"
-          autoHeight
-          style={{ paddingVertical: 5 }}
-          onChangeText={(value) => setDesEvent(value?.trim())}
-        />
-        <Text>Ghi chú:</Text>
-        <InputItem
-          placeholder="Ghi chú"
-          multiline
-          onChangeText={(value) => setNoteEvent(value?.trim())}
-        ></InputItem>
-      </View>,
-      [
-        { text: "Thoát", onPress: () => console.log("cancel"), style: "cancel" },
-        { text: "Thêm", onPress: () => console.log("ok") },
-      ]
-    );
+    console.log("nameEvent", nameEvent, dateEvent, desEvent, noteEvent);
+    insertANewEvent(nameEvent, dateEvent, desEvent, noteEvent).then((isRes) => {
+      setIsLoading(true);
+      if (isRes) {
+        Toast.success("Thêm sự kiện mới thành công!");
+      } else {
+        Toast.fail("Thất bại!");
+      }
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 200);
+    });
   };
+
+  // if (isLoading)
+  //   return (
+  //     <View
+  //       style={{
+  //         display: "flex",
+  //         justifyContent: "center",
+  //         alignItems: "center",
+  //       }}
+  //     >
+  //       <Text>
+  //         Loading <ActivityIndicator />
+  //       </Text>
+  //     </View>
+  //   );
+
   return (
     <View
       style={{
@@ -132,30 +177,124 @@ const ProcessBaby = ({ listAccountBaby, nameRouteUserId }: Props) => {
       >
         <CardHeader
           title={
-            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-              <Text style={{ color: "#1870bc", fontSize: 16, fontWeight: "bold" }}>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <Text
+                style={{ color: "#1870bc", fontSize: 16, fontWeight: "bold" }}
+              >
                 Quá trình phát triển:
               </Text>
-              <Button size="small" onPress={() => handleAddEvent()}>
+              <Button size="small" onPress={() => setShowEvent(true)}>
                 Thêm sự kiện
               </Button>
             </View>
           }
         ></CardHeader>
         <CardBody>
-          <WingBlank size="lg">
-            <Steps size="large">
-              <Step
-                title={<Text> {isFirstDay}</Text>}
-                description={"nhịp đập đầu tiên!"}
-                status={"finish"}
-              />
-              <Step title={<Text> {now}</Text>} description={"Hôm nay"} status={"success"} />
-              <Step title={<Text> {isBirthday}</Text>} description={"Oa oa"} status={"wait"} />
-            </Steps>
-          </WingBlank>
+          <ScrollView>
+            <View>
+              <WingBlank size="lg">
+                <Steps size="large">
+                  {listEventCook &&
+                    listEventCook?.length > 0 &&
+                    listEventCook?.map((item, index) => {
+                      return (
+                        <Step
+                          key={item?.id}
+                          title={<Text> {item?.event}</Text>}
+                          description={item?.description}
+                          status={item?.status || "finish"}
+                        />
+                      );
+                    })}
+                </Steps>
+              </WingBlank>
+            </View>
+          </ScrollView>
         </CardBody>
       </Card>
+      <Modal
+        style={{
+          width: windowWidth - 10,
+          // height: windowHeight - 100,
+          marginTop: 100,
+        }}
+        visible={isShowEvent}
+        transparent
+        animationType="fade"
+        title={
+          <Text style={{ color: "#1870bc", fontSize: 16, fontWeight: "bold" }}>
+            Thêm sự kiện
+          </Text>
+        }
+        footer={[
+          {
+            text: "Thoát",
+            onPress: () => setShowEvent(false),
+            style: "cancel",
+          },
+          {
+            text: "Thêm",
+            onPress: () => {
+              setShowEvent(false);
+              handleAddEvent();
+            },
+          },
+        ]}
+      >
+        <View
+          style={{
+            width: windowWidth,
+            display: "flex",
+            justifyContent: "flex-start",
+            marginTop: 10,
+          }}
+        >
+          <Text style={{ fontSize: 14, fontWeight: "bold", paddingTop: 10 }}>
+            Tên sự kiện:
+          </Text>
+          <InputItem
+            placeholder="Tên sự kiện"
+            multiline
+            textBreakStrategy="highQuality"
+            onChangeText={(value) => setNameEvent(value?.trim())}
+            style={{ borderBottomWidth: 1, borderColor: "#1870bc" }}
+          ></InputItem>
+          <Text style={{ fontSize: 14, fontWeight: "bold", paddingTop: 10 }}>
+            Ngày xảy ra sự kiện:
+          </Text>
+          <InputItem
+            placeholder="Ngày xảy ra sự kiện"
+            onChangeText={(value) => setDateEvent(value?.trim())}
+            style={{ borderBottomWidth: 1, borderColor: "#1870bc" }}
+          ></InputItem>
+          <Text style={{ fontSize: 14, fontWeight: "bold", paddingTop: 10 }}>
+            Mô tả sự kiện:
+          </Text>
+          <TextareaItem
+            placeholder="Mô tả sự kiện"
+            autoHeight
+            rows={10}
+            style={{
+              marginLeft: 15,
+              paddingLeft: -10,
+              borderBottomWidth: 1,
+              borderColor: "#1870bc",
+            }}
+            onChangeText={(value) => setDesEvent(value?.trim())}
+          />
+          <Text style={{ fontSize: 14, fontWeight: "bold", paddingTop: 10 }}>
+            Ghi chú:
+          </Text>
+          <InputItem
+            placeholder="Ghi chú"
+            multiline
+            onChangeText={(value) => setNoteEvent(value?.trim())}
+            style={{ borderBottomWidth: 1, borderColor: "#1870bc" }}
+          ></InputItem>
+        </View>
+      </Modal>
     </View>
   );
 };
