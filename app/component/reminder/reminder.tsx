2ea1b5d4639cb17808fd235c07bc92917251c1a7
Oscar.To <oscar.to@grapecity.com>
2023-08-29 16:48:05 +0900

    fix: Reminder screen

--- app/component/reminder/reminder.tsx
+++ app/component/reminder/reminder.tsx
@@ -1,41 +1,39 @@
-import React, { useEffect, useState } from "react";
-import DateTimePicker from "@react-native-community/datetimepicker";
-import { Button, Modal, Tag, Text, View, WhiteSpace } from "@ant-design/react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+
+import _ from "lodash";
+import moment from "moment";
+
+import {
+  ActivityIndicator,
+  Button,
+  Result,
+  Text,
+  View,
+  WhiteSpace,
+} from "@ant-design/react-native";
+
 import * as Calendar from "expo-calendar";
 import CalendarStrip from "react-native-calendar-strip";
-import moment from "moment";
-import { Alert, Platform, useWindowDimensions } from "react-native";
-import Input from "@ant-design/react-native/lib/input-item/Input";
-import * as Localization from "expo-localization";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, GestureHandlerRootView } from "react-native-gesture-handler";
+
+import { ImageBackground, useWindowDimensions } from "react-native";
+
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faAdd, faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
-import { ScrollView } from "react-native-gesture-handler";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import ModalAddEvent from "./sub-component/modal-add-event";
 
 const nameCalenderSource = "HiMomGoogleCalendar";
-const defaultCalendarSource = {
-  type: "",
-  isLocalAccount: true,
-  name: nameCalenderSource,
-}; //only for android
-
-const newCalendar = {
-  title: "HiMomEvent",
-  entityType: Calendar.EntityTypes.EVENT,
-  color: "#2196F3",
-  source: defaultCalendarSource,
-  name: "internal",
-  accessLevel: Calendar.CalendarAccessLevel.OWNER,
-  ownerAccount: "personal",
-};
 
 const ReminderComponent = () => {
+  const image = require("../../../assets/background.jpg");
   const [todoList, setTodoList] = useState([]);
-  const [valueNote, setValueNote] = useState("");
-  const [valueNoteAdd, setValueNoteAdd] = useState("");
+  const [isShowEvent, setShowEvent] = useState(false);
+  const [isDisableButtonEvent, setDisableButtonEvent] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const { width, height } = useWindowDimensions();
-  const [selectedTask, setSelectedTask] = useState(null);
 
   const [currentDate, setCurrentDate] = useState<any>(
     `${moment().format("YYYY")}-${moment().format("MM")}-${moment().format("DD")}`
@@ -44,198 +42,228 @@ const ReminderComponent = () => {
   const [status, requestPermission] = Calendar.useCalendarPermissions();
   Calendar.getCalendarPermissionsAsync();
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
   const datesWhitelist = [
     {
-      start: moment(),
+      start: moment().subtract(10, "days"),
       end: moment().add(365, "days"), // total 4 days enabled
     },
   ];
 
-  const getAllEvent = async () => {
-    await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT).then(async (item) => {
-      if (item && item.length > 0) {
-        const calendarIdExisted = item?.find(
-          (childItem) => childItem?.source?.name === nameCalenderSource
-        );
-        let calendarId = calendarIdExisted?.id;
-        console.log(moment(currentDate).endOf("day").toDate());
-        const listEvent = await Calendar.getEventsAsync(
-          [calendarId],
-          moment(currentDate).startOf("day").toDate(),
-          moment(currentDate).endOf("day").toDate()
-        );
-        console.log("listEvent", listEvent);
-        setTodoList(listEvent);
-      }
-    });
-  };
-  const addEventToCalender = () => {
-    Modal.alert(
-      "Thêm sự kiện",
-      <View>
-        <Text> Ngày xảy ra sự kiện:</Text>
-        <Input value={currentDate} editable={false} />
-        <Text> Nội dung cần nhắc nhở:</Text>
-        <Input
-          placeholder="nhập nội dung bạn muốn nhắc nhở"
-          defaultValue={valueNote}
-          onChangeText={(value) => setValueNote(value)}
-        />
-        <Text> Ghi chú thêm:</Text>
-        <Input
-          placeholder="nhập nội dung bạn muốn ghi chú"
-          defaultValue={valueNoteAdd}
-          onChangeText={(value) => setValueNoteAdd(value)}
-        />
-      </View>,
-      [
-        {
-          text: "Thoát",
-        },
-        {
-          text: "Đồng ý",
-          onPress: async () => {
-            //Check if has Calender
-            let calendarId: any;
-            await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT)
-              .then((item) => {
-                const calendarIdExisted = item?.find(
-                  (childItem) => childItem?.source?.name === nameCalenderSource
-                );
-                const checkExist = item?.some(
-                  (childItem) => childItem?.source?.name === nameCalenderSource
-                );
+  const getAllEvent = useCallback(
+    async (selectedDate) => {
+      setIsLoading(true);
+      let listEvent: any;
+      await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT)
+        .then(async (item) => {
+          if (item && item.length > 0) {
+            const calendarIdExisted = item?.find(
+              (childItem) => childItem?.source?.name === nameCalenderSource
+            );
+            let calendarId = calendarIdExisted?.id;
+            listEvent = await Calendar.getEventsAsync(
+              [calendarId],
+              moment(selectedDate).startOf("day").toDate(),
+              moment(selectedDate).endOf("day").toDate()
+            );
+            setTodoList(listEvent);
+          }
+        })
+        .then(() => {
+          console.log("todoList", listEvent);
+          setTodoList(listEvent);
+          setIsLoading(false);
+        });
+    },
+    [currentDate, todoList]
+  );
 
-                if (checkExist) {
-                  calendarId = calendarIdExisted.id;
-                } else {
-                  calendarId = Calendar.createCalendarAsync(newCalendar);
-                }
-              })
-              .then(() => {
-                const event = {
-                  title: `HiMom: ${valueNote}`,
-                  notes: calendarId.toString(),
-                  startDate: new Date(currentDate),
-                  endDate: new Date(currentDate),
-                  timeZone: Localization.timezone,
-                };
-                console.log(calendarId.toString());
-                Calendar.createEventAsync(calendarId.toString(), event);
-              });
-          },
-        },
-      ]
-    );
-  };
+  useEffect(() => {
+    requestPermission();
+  }, [todoList]);
 
   return (
     <GestureHandlerRootView>
       <SafeAreaView>
-        <CalendarStrip
-          headerText="Nhắc nhở"
-          calendarAnimation={{ type: "sequence", duration: 30 }}
-          style={{
-            height: 150,
-            paddingTop: 20,
-            paddingBottom: 20,
-          }}
-          calendarHeaderStyle={{ color: "#000000" }}
-          dateNumberStyle={{ color: "#000000", paddingTop: 10 }}
-          dateNameStyle={{ color: "#BBBBBB" }}
-          highlightDateNumberStyle={{
-            color: "#fff",
-            backgroundColor: "#2E66E7",
-            marginTop: 10,
-            height: 35,
-            width: 35,
-            textAlign: "center",
-            borderRadius: 17.5,
-            overflow: "hidden",
-            paddingTop: 6,
-            fontWeight: "600",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          highlightDateNameStyle={{ color: "#2E66E7" }}
-          disabledDateNameStyle={{ color: "grey" }}
-          disabledDateNumberStyle={{ color: "grey", paddingTop: 10 }}
-          iconContainer={{ flex: 0.1 }}
-          datesWhitelist={datesWhitelist}
-          selectedDate={currentDate}
-          onDateSelected={(date) => {
-            const selectedDate = `${moment(date).format("YYYY")}-${moment(date).format(
-              "MM"
-            )}-${moment(date).format("DD")}`;
-            setCurrentDate(selectedDate);
-            getAllEvent();
-          }}
-          locale={{
-            name: "vn",
-            config: {
-              weekdaysShort: "CN_T2_T3_T4_T5_T6_T7".split("_"),
-              weekdaysMin: "CN_T2_T3_T4_T5_T6_T7".split("_"),
-            },
-          }}
-        />
+        <ImageBackground source={image} resizeMode="cover" style={{ width: width, height: height }}>
+          <ModalAddEvent
+            currentDate={currentDate}
+            isShowEvent={isShowEvent}
+            setShowEvent={() => setShowEvent(false)}
+            nameCalenderSource={nameCalenderSource}
+          />
+          <CalendarStrip
+            headerText="Nhắc nhở"
+            calendarAnimation={{ type: "sequence", duration: 30 }}
+            style={{
+              height: 150,
+              paddingTop: 20,
+            }}
+            calendarHeaderStyle={{
+              color: "#1870bc",
+              fontSize: 16,
+              fontWeight: "bold",
+            }}
+            dateNumberStyle={{ color: "#000000", paddingTop: 10 }}
+            dateNameStyle={{ color: "#BBBBBB" }}
+            highlightDateNumberStyle={{
+              color: "#fff",
+              backgroundColor: "#1870bc",
+              marginTop: 10,
+              height: 35,
+              width: 35,
+              textAlign: "center",
+              borderRadius: 17.5,
+              overflow: "hidden",
+              paddingTop: 6,
+              fontWeight: "600",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            highlightDateNameStyle={{ color: "#1870bc", fontSize: 13 }}
+            disabledDateNameStyle={{ color: "grey" }}
+            disabledDateNumberStyle={{ color: "grey", paddingTop: 10 }}
+            iconContainer={{ flex: 0.1 }}
+            datesWhitelist={datesWhitelist}
+            selectedDate={currentDate}
+            onDateSelected={(date) => {
+              const selectedDate = `${moment(date).format("YYYY")}-${moment(date).format(
+                "MM"
+              )}-${moment(date).format("DD")}`;
+              setCurrentDate(selectedDate);
 
-        {todoList?.length > 0 && (
-          <View style={{ width: width, height: height - 280 }}>
-            <ScrollView>
-              {todoList?.map((item) => (
-                <View
-                  style={{
-                    width: width - 10,
-                    height: 40,
-                    backgroundColor: "white",
-                    borderColor: "#2e66e7",
-                    marginLeft: 5,
-                    marginBottom: 10,
-                    borderRadius: 20,
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <View>
-                    <Text style={{ color: "black", paddingLeft: 20, paddingTop: 5 }}>
-                      Sự kiện: {String(item?.title).replaceAll("HiMom:", "")}
-                    </Text>
-                    <Text style={{ color: "black", paddingLeft: 20 }}>{item?.note}</Text>
-                  </View>
-                  <Text
-                    onPress={() => Calendar.openEventInCalendar(item.id)}
-                    style={{ margin: 10 }}
-                  >
-                    <FontAwesomeIcon icon={faArrowCircleRight} color="black" />
-                  </Text>
-                </View>
-              ))}
-            </ScrollView>
-          </View>
-        )}
-        <View
-          style={{
-            width: width - 50,
-            bottom: 0,
-            marginBottom: 0,
-            display: "flex",
-            // position: "absolute",
-            alignSelf: "center",
-          }}
-        >
-          <Button
-            type="ghost"
-            style={{ width: width - 50, backgroundColor: "white" }}
-            onPress={() => addEventToCalender()}
+              getAllEvent(selectedDate);
+              if (moment(date) >= moment().subtract(1, "d")) {
+                setDisableButtonEvent(false);
+              } else {
+                setDisableButtonEvent(true);
+              }
+            }}
+            locale={{
+              name: "vi",
+              config: {
+                weekdaysShort: "CN_T2_T3_T4_T5_T6_T7".split("_"),
+                weekdaysMin: "CN_T2_T3_T4_T5_T6_T7".split("_"),
+              },
+            }}
+          />
+          <Text style={{ paddingLeft: 10, fontWeight: "bold" }}>{`Hôm nay: ${moment().format(
+            "DD/MM/YYYY hh:mm"
+          )}`}</Text>
+          <WhiteSpace />
+          <View
+            style={{
+              width: width - 20,
+              display: "flex",
+              alignSelf: "center",
+              justifyContent: "space-between",
+              flexDirection: "row",
+              marginBottom: 10,
+            }}
           >
-            <FontAwesomeIcon icon={faAdd} />
-            <Text>Thêm sự kiện</Text>
-          </Button>
-        </View>
+            <Text style={{ fontSize: 16, fontWeight: "bold", color: "#1870bc" }}>
+              Danh sách sự kiện:
+            </Text>
+            <Button
+              type="ghost"
+              size="small"
+              style={{ width: 110, backgroundColor: "transparent" }}
+              onPress={() => setShowEvent(true)}
+              disabled={isDisableButtonEvent}
+            >
+              <FontAwesomeIcon icon={faAdd} />
+              <Text style={{ fontSize: 13, fontWeight: "500" }}>Thêm sự kiện</Text>
+            </Button>
+          </View>
+          {isLoading ? (
+            <View
+              style={{
+                backgroundColor: "transparent",
+                alignContent: "center",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {isLoading && (
+                <ActivityIndicator size="small" color="#1870bc" text="Tải dữ liệu..." />
+              )}
+            </View>
+          ) : (
+            <View>
+              {todoList?.length > 0 && !isLoading ? (
+                <View
+                  style={{ width: width, height: height - 280, backgroundColor: "transparent" }}
+                >
+                  <ScrollView style={{ backgroundColor: "transparent" }}>
+                    {todoList?.map((item) => (
+                      <View
+                        style={{
+                          width: width - 10,
+                          height: 40,
+                          backgroundColor: "transparent",
+                          borderColor: "#1870bc",
+                          borderWidth: 1,
+                          marginLeft: 5,
+                          marginTop: 10,
+                          borderRadius: 20,
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <View
+                          style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            marginLeft: 10,
+                          }}
+                        >
+                          <Text
+                            style={{
+                              color: "black",
+                              width: 35,
+                              fontSize: 13,
+                              fontWeight: "500",
+                              marginRight: 10,
+                            }}
+                          >
+                            {String(moment(item?.startDate).format("hh"))}:
+                            {moment(item?.startDate).format("mm")}
+                          </Text>
+                          <Text style={{ borderRightWidth: 1, borderColor: "#1870bc" }}></Text>
+                          <Text
+                            style={{
+                              color: "black",
+                              paddingLeft: 10,
+                              fontSize: 15,
+                              fontWeight: "600",
+                            }}
+                          >
+                            {String(item?.title).replaceAll("HiMom:", "")}
+                          </Text>
+                        </View>
+                        <Text
+                          onPress={() => Calendar.openEventInCalendar(item.id)}
+                          style={{ margin: 10 }}
+                        >
+                          <FontAwesomeIcon icon={faArrowCircleRight} color="#1870bc" />
+                        </Text>
+                      </View>
+                    ))}
+                  </ScrollView>
+                </View>
+              ) : (
+                <Result
+                  imgUrl={{
+                    uri: "https://zos.alipayobjects.com/rmsportal/GcBguhrOdlYvGfnsXgrE.png",
+                  }}
+                  title=""
+                  message="Không có ghi chú"
+                  style={{ backgroundColor: "transparent" }}
+                />
+              )}
+            </View>
+          )}
+        </ImageBackground>
       </SafeAreaView>
     </GestureHandlerRootView>
   );
---
+++ app/component/reminder/sub-component/modal-add-event.tsx
@@ -0,0 +1,224 @@
+import React, { useEffect, useState } from "react";
+
+import moment from "moment";
+import * as Calendar from "expo-calendar";
+import * as Localization from "expo-localization";
+
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { Modal, View, Text, Button, Toast } from "@ant-design/react-native";
+
+import Input from "@ant-design/react-native/lib/input-item/Input";
+import DateTimePicker from "@react-native-community/datetimepicker";
+
+interface Props {
+  currentDate: string;
+  isShowEvent?: boolean;
+  setShowEvent?: () => void;
+  nameCalenderSource: string;
+}
+const ModalAddEvent = ({
+  currentDate,
+  isShowEvent = false,
+  setShowEvent,
+  nameCalenderSource,
+}: Props) => {
+  const { width, height } = useWindowDimensions();
+  const [timePicker, setTimePicker] = useState<any>(moment(currentDate).toDate());
+  const [valueNote, setValueNote] = useState("");
+  const [valueNoteAdd, setValueNoteAdd] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [isShowDatePicker, setShowDatePicker] = useState(false);
+
+  const defaultCalendarSource = {
+    type: "",
+    isLocalAccount: true,
+    name: nameCalenderSource,
+  }; //only for android
+
+  const newCalendar = {
+    title: "HiMomEvent",
+    entityType: Calendar.EntityTypes.EVENT,
+    color: "#2196F3",
+    source: defaultCalendarSource,
+    name: "internal",
+    accessLevel: Calendar.CalendarAccessLevel.OWNER,
+    ownerAccount: "personal",
+  };
+
+  const styles = StyleSheet.create({
+    input: {
+      flexDirection: "row",
+      padding: 5,
+      borderWidth: 1,
+      borderColor: "#1870bc",
+      borderRadius: 10,
+      width: width - 30,
+      marginTop: 10,
+    },
+    inputError: {
+      flexDirection: "row",
+      padding: 5,
+      borderWidth: 2,
+      borderColor: "red",
+      borderRadius: 10,
+      width: width - 30,
+      marginTop: 10,
+    },
+    inputItem: {
+      width: width - 35,
+      padding: 4,
+      fontSize: 14,
+      fontWeight: "600",
+    },
+  });
+
+  useEffect(() => {
+    setValueNote("");
+    setValueNoteAdd("");
+  }, [isShowEvent]);
+
+  const handleAddEvent = async () => {
+    if (valueNote?.trim() === "") {
+      setIsError(true);
+    } else {
+      setIsError(false);
+
+      //Check if has Calender
+      let calendarId: any;
+      await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT)
+        .then((item) => {
+          const calendarIdExisted = item?.find(
+            (childItem) => childItem?.source?.name === nameCalenderSource
+          );
+          const checkExist = item?.some(
+            (childItem) => childItem?.source?.name === nameCalenderSource
+          );
+
+          if (checkExist) {
+            calendarId = calendarIdExisted.id;
+          } else {
+            calendarId = Calendar.createCalendarAsync(newCalendar);
+          }
+        })
+        .then(() => {
+          console.log(timePicker, moment(timePicker).toDate());
+          const event = {
+            title: `HiMom: ${valueNote}`,
+            notes: `${calendarId.toString()}: ${valueNoteAdd}`,
+            startDate: moment(timePicker).toDate(),
+            endDate: moment(timePicker).toDate(),
+            timeZone: Localization.timezone,
+          };
+          Calendar.createEventAsync(calendarId.toString(), event).then((isRes) => {
+            if (isRes) Toast.success("Đã thêm thành công!");
+          });
+        });
+    }
+  };
+
+  return (
+    <View>
+      {isShowDatePicker && (
+        <DateTimePicker
+          display="clock"
+          mode="time"
+          is24Hour={true}
+          value={moment(currentDate).toDate() || new Date()}
+          maximumDate={moment(currentDate).endOf("day").toDate()}
+          minimumDate={moment(currentDate).toDate()}
+          negativeButton={{ label: "Thoát", textColor: "black" }}
+          positiveButton={{ label: "Chọn" }}
+          onChange={(value) => {
+            if (moment(value?.nativeEvent?.timestamp) < moment()) {
+              Toast.fail("Không thể chọn thời gian đã trôi qua! Hãy chọn lại!");
+            } else {
+              setShowDatePicker(false);
+              setTimePicker(value?.nativeEvent?.timestamp);
+            }
+          }}
+          onError={() => Toast.fail("Vui lòng chọn lại!")}
+        />
+      )}
+      <Modal
+        style={{
+          width: width - 10,
+        }}
+        popup={true}
+        visible={isShowEvent}
+        transparent
+        animationType="fade"
+        title={
+          <View
+            style={{
+              width: width - 10,
+              display: "flex",
+              justifyContent: "center",
+              alignContent: "center",
+              alignItems: "center",
+              alignSelf: "center",
+              textAlign: "center",
+            }}
+          >
+            <Text style={{ color: "#1870bc", fontSize: 16, fontWeight: "bold" }}>
+              Thêm sự kiện: {moment(currentDate).format("DD/MM/YYYY")}
+            </Text>
+            <Button type="ghost" size="small" onPress={() => setShowDatePicker(true)}>
+              <Text>{moment(timePicker).format("hh:mm")}</Text>
+            </Button>
+          </View>
+        }
+        footer={[
+          {
+            text: "Thoát",
+            onPress: () => setShowEvent(),
+            style: "cancel",
+          },
+          {
+            text: "Thêm",
+            onPress: () => {
+              if (valueNote?.trim() === "") {
+                setIsError(true);
+              } else {
+                setIsError(false);
+                setShowEvent();
+                handleAddEvent();
+              }
+            },
+          },
+        ]}
+      >
+        <View style={{ width: width - 30, paddingTop: 10 }}>
+          <View style={isError ? styles.inputError : styles.input}>
+            <Input
+              style={styles.inputItem}
+              maxLength={50}
+              placeholder={
+                isError
+                  ? "Không được phép bỏ qua trường thông tin này. Hãy nhập nội dung cần nhắc nhở!"
+                  : "Nội dung cần nhắc nhở"
+              }
+              multiline={isError}
+              defaultValue={valueNote}
+              onChangeText={(value) => {
+                setIsError(false);
+                setValueNote(value);
+              }}
+            />
+          </View>
+          <View style={styles.input}>
+            <Input
+              style={styles.inputItem}
+              multiline
+              maxLength={204}
+              placeholder="Nội dung cần ghi chú"
+              defaultValue={valueNoteAdd}
+              onChangeText={(value) => setValueNoteAdd(value)}
+            />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default ModalAddEvent;
